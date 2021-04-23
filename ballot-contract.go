2d278339fcd7694c05baa387b079b6a6befa5d27
Kevin <weiyih@sheridancollege.ca>
2021-04-22 21:50:38 -0400

    cleaned up. changed crc function to return hexidecimal string

--- ballot-contract.go
+++ ballot-contract.go
@@ -20,12 +20,13 @@ type BallotContract struct {
 // BallotExists returns true when asset with given ID exists in world state
 func (s *BallotContract) BallotExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
 	hashId := hash(id)
-	assetJSON, err := ctx.GetStub().GetState(hashId)
+	data, err := ctx.GetStub().GetState(hashId)
+
 	if err != nil {
-		return false, fmt.Errorf("Failed to read from world state: %v", err)
+		return false, err
 	}
 
-	return assetJSON != nil, nil
+	return data != nil, nil
 }
 
 // CreateBallot
@@ -36,20 +37,19 @@ func (s *BallotContract) CreateBallot(ctx contractapi.TransactionContextInterfac
 	// Checks if ballot already exists
 	exists, err := s.BallotExists(ctx, id)
 	if err != nil {
-		return fmt.Errorf("Error - Could not read from world state. %s", err)
+		return fmt.Errorf("could not read from world state: %s", err)
+	} else if exists {
+		return fmt.Errorf("ballot already exists")
 	}
-	if exists {
-		return fmt.Errorf("Error - Ballot %s already exists", id)
-	}
-	
+
 	hashId := hash(id)
 	// Create vote Object
 	ballot := Ballot{
-		Id: 			hashId,
-		ElectionId: 	electionId,
-		DistrictId:     districtId,
-		CandidateId:   	candidateId,
-		Timestamp:  	timestamp,
+		Id:          hashId,
+		ElectionId:  electionId,
+		DistrictId:  districtId,
+		CandidateId: candidateId,
+		Timestamp:   timestamp,
 	}
 
 	ballotJSON, err := json.Marshal(ballot)
@@ -60,14 +60,13 @@ func (s *BallotContract) CreateBallot(ctx contractapi.TransactionContextInterfac
 	return ctx.GetStub().PutState(hashId, ballotJSON)
 }
 
-// Returns the timestamp of a ballot if it exists
+// Returns the ballot if it exists
 func (s *BallotContract) ReadBallot(ctx contractapi.TransactionContextInterface, id string) (*Ballot, error) {
 	exists, err := s.BallotExists(ctx, id)
 	if err != nil {
-		return nil, fmt.Errorf("Error - Could not read from world state. %s", err)
-	}
-	if exists {
-		return nil, fmt.Errorf("Error - Ballot %s already exists", id)
+		return nil, fmt.Errorf("could not read from world state. %s", err)
+	} else if !exists {
+		return nil, fmt.Errorf("the ballot does not exist")
 	}
 
 	hashId := hash(id)
@@ -78,7 +77,7 @@ func (s *BallotContract) ReadBallot(ctx contractapi.TransactionContextInterface,
 	err = json.Unmarshal(bytes, ballot)
 
 	if err != nil {
-		return nil, fmt.Errorf("Could not unmarshal world state data to type Ballot")
+		return nil, fmt.Errorf("could not unmarshal world state data to type Ballot")
 	}
 
 	return ballot, nil
@@ -119,13 +118,13 @@ func (s *BallotContract) GetAllBallots(ctx contractapi.TransactionContextInterfa
 
 // https://softwareengineering.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed
 // https://golang.org/pkg/hash/crc32
-func hash(id string) string {
+func hash(stringId string) string {
 	// Koopman's polynomial.
-    // Also has better error detection characteristics than IEEE.
-    // https://dx.doi.org/10.1109/DSN.2002.1028931
+	// Also has better error detection characteristics than IEEE.
+	// https://dx.doi.org/10.1109/DSN.2002.1028931
 	koopmanTable := crc32.MakeTable(crc32.Koopman)
-	b := []byte(id)
-	hash := crc32.Checksum(b, koopmanTable)
-	
-	return string(hash)
+	b := []byte(stringId)
+	checksum := crc32.Checksum(b, koopmanTable)
+	hexString := fmt.Sprintf("%x", checksum)
+	return hexString
 }
